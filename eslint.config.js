08c751dd4bf62781f4bd5b4a28d5771594e2c8cd
Lu-Jiejie <lu-jiejie@outlook.com>
2025-08-27 05:14:45 +0800

    chore: update pkgs and readme

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,9 @@ export default antfu(
   {
     unocss: false,
     formatters: true,
-    pnpm: true,
   },
 ).removeRules(
   'no-irregular-whitespace',
-  'vue/no-irregular-whitespace',
   'no-console',
   'no-alert',
 )
